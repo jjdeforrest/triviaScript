bc8e7dec6af2546cf319d0a489bc29804a42321e
jjdeforrest <jjdeforrest@users.noreply.github.com>
2019-11-22 17:28:10 -0800

    playingwithgamertagform

---
+++ JS/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function gamerTag () {
+  var gamer = prompt('What is your Gamer Tag!!!');
+  var user = gamer;
+  alert('welcome to the game ' + user);
+  console.log(user);
+  return user;
+}
