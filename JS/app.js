460d877758ef1dcd43a011726bc7879c5a02c920
Jerome Joof <jeromejoof@yahoo.com>
2019-11-25 15:09:53 -0800

    working on questions and answers logic

--- JS/app.js
+++ JS/app.js
@@ -51,33 +51,33 @@ function showQuiz(questions, quizContainer, scoresContainer, submitButton){
 
   function showQuestions(questions, quizContainer){
 
-    var outcome = [];
-    var answers;
     var letter;
 
     // we loop through each questions
     for(var i = 0; i < questions.length; i++){
 
-      // the current list of answers should be an empty array
-      answers = [];
+      var currentQuestionElem = document.getElementById('questions' + i);
 
+      var questionsLegendElem = document.createElement('legend');
+      console.log(questionsLegendElem);
+      
+      questionsLegendElem.textContent = questions[i].question;
+      currentQuestionElem.appendChild(questionsLegendElem);
 
-      // loop through each answer available to a question
+      //   // loop through each answer available to a question
       for(letter in questions[i].answers){
 
-        // add an html checkbox
-        answers.push(
-          '<label>' + '<input type="checkbox" name="question ' + i + ' " value=" ' + letter + ' "> ' + letter + ': ' + questions[i].answers[letter] + '</label>'
-        );
+      var questionsLabelElem = document.createElement('label');
+      questionsLabelElem.textContent = letter + ': ' + questions[i].answers[letter];
+      currentQuestionElem.appendChild(questionsLabelElem);
+
+      var inputtElem = document.createElement('INPUT');
+      questionsLabelElem.appendChild(inputtElem);
+      inputtElem.setAttribute('type', 'radio');
+      
       }
 
-      // pushing the question and the answer to the outcome
-      outcome.push(
-        '<div class="question">' + questions[i].question + '</div>' + '<div class="answers">' + answers.join('') + '</div>'
-      );
-
     }
-    quizContainer.innerHTML = outcome.join('');
   }
 
   function showScores(questions, quizContainer, scoresContainer){
@@ -91,8 +91,12 @@ function showQuiz(questions, quizContainer, scoresContainer, submitButton){
     // loop through each question
     for(var i = 0; i < questions.length; i++){
 
+      console.log(answerContainers);
       // target the selected answer
-      userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+      var currentContainer = answerContainers[i];
+      console.log('----------', currentContainer);
+      userAnswer = (currentContainer.querySelector('input[name=question'+i+']:checked'));
+      console.log(userAnswer);
 
       // conditional to check for correct answers
       if(userAnswer === questions[i].correctAnswer){
@@ -113,148 +117,19 @@ function showQuiz(questions, quizContainer, scoresContainer, submitButton){
 
   // when the user clicks on the submit button it shows them their score
 
-  function clickHandler(){
-    showScores(questions, quizContainer, scoresContainer);
+  // function clickHandler(){
+  //   showScores(questions, quizContainer, scoresContainer);
 
-  }
+  // }
 
-  submitButton.addEventListener('click', clickHandler);
+  // submitButton.addEventListener('click', clickHandler);
+}
 
-  // clickHandler();
+function submitHandler(event){
+
+  event.preventDefault();
+  console.log('submitHandler');
 
 }
 
-  ///////////////////////////////////////////////////
- /////////   Famous People Category      ///////////
-///////////////////////////////////////////////////
-
-
-//creating object for identifying famous people
-// function IdentifyFamousPeople({ question = 'Who is this?' }, cropPicture, options, answer, fullPicture) {
-//     this.question = question;
-//     this.cropPicture = cropPicture;
-//     this.options = options;
-//     this.answer = answer;
-//     this.fullPicture = fullPicture;
-// }
-
-// var questionNumber = 0;
-// var askQuestion = [];
-// //adding all the objects to array
-// var questionsArr = [];
-// questionsArr.push(new IdentifyFamousPeople(' ', 'img/bradley.jpg', ['Bradley Cooper', 'Leonardo Di Caprio', 'Nicholas Cage'], 'option_1', 'img/bradleyFull.jpg'));
-// questionsArr.push(new IdentifyFamousPeople(' ', 'img/dwayne.jpg', ['Robert Di Nero', 'Dwayne Johnson', 'Hugh Jackman'], 'option_2', 'img/dwayneFull.jpg'));
-// questionsArr.push(new IdentifyFamousPeople(' ', 'img/gaga.jpg', ['Miley Cyrus', 'Madonna', 'Lady Gaga'], 'option_3', 'img/gagaFull.jpg'));
-// questionsArr.push(new IdentifyFamousPeople('', 'img/messi.jpg', ['Christiano Ronaldo', 'Lionel Messi', 'David Beckham'], 'option_2', 'img/messiFull.jpg'));
-// questionsArr.push(new IdentifyFamousPeople(' ', 'img/jennifer.jpg', ['Jennifer Lopez', 'Penelope Cruz', 'Jennifer Aniston'], 'option_1', 'img/jenniferFull.jpg'));
-
-// var famousPeopleContainer = document.getElementById('famous_people');
-
-// function getQuestion() {
-//     for (var i = questionNumber; i < questionsArr.length; i++ ){
-//         //Question Element
-//         var question = addTextElement('h2', 'id', 'question', famousPeopleContainer, questionsArr[i].question);
-        
-//         //Image Element
-//         var cropImage = addImage('img', 'id', 'crop_image', famousPeopleContainer, questionsArr[i].cropPicture);
-        
-//         //Answer option element
-//         var ulElement = addTextElement('ul', 'id', 'ul_element', famousPeopleContainer);
-        
-//         //display all three answer options
-//         for (var j = 0; j < questionsArr[i].options.length; j++) {
-//             var option = addTextElement('li', 'id', `option_${j + 1}`, ulElement, questionsArr[i].options[j]);
-//         }
-//         break;       
-//     }
-
-//     console.log('questionNumber',questionNumber)
-// }
-// getQuestion();
-
-
-
-
-// //function to get text element
-// function addTextElement(tag, attribute, value, famousPeopleContainer, text) {
-//     var element = document.createElement(tag);
-//     element.setAttribute(attribute, value);
-//     famousPeopleContainer.appendChild(element);
-//     element.textContent = text;
-//     return element;
-// }
-
-// //function to get image
-// function addImage(tag, attribute, value, famousPeopleContainer, image) {
-//     var element = document.createElement(tag);
-//     element.setAttribute(attribute, value);
-//     famousPeopleContainer.appendChild(element);
-//     element.src = image;
-//     return element;
-// }
-
-// //get options and add event listeners
-// function getOptions() {
-//     //get three options and store in variable
-//     var option1 = document.getElementById('option_1');
-//     var option2 = document.getElementById('option_2');
-//     var option3 = document.getElementById('option_3');
-    
-//     //listen to the click event for each option
-//     option1.addEventListener('click', clickHandler);
-//     option2.addEventListener('click', clickHandler);
-//     option3.addEventListener('click', clickHandler);
-// }
-// getOptions();
-
-// // respond to the click on the options
-// function clickHandler(event1) {
-//     var id = event1.target.id;
-
-//         if (id === questionsArr[questionNumber].answer) {
-//             //display answer
-//             addTextElement('h3', 'id', 'answer', famousPeopleContainer, 'Correct!');
-//             //display full image
-//             addImage('img', 'id', 'full_image', famousPeopleContainer, questionsArr[questionNumber].fullPicture);
-//         }
-//         else {
-//             //display answer
-//             addTextElement('h3', 'id', 'answer', famousPeopleContainer, 'Incorrect!  YOU SUCK!');
-//             //display full image
-//             addImage('img', 'id', 'full_image', famousPeopleContainer, questionsArr[questionNumber].fullPicture);
-    
-//         }
-//         questionNumber++;
-//         var button  = addTextElement('button', 'id', 'next_button', famousPeopleContainer, 'Next');
-
-//         // debugger;
-//         button.addEventListener('click', nextQuestion);
-//         option1.removeEventListener('click', clickHandler);
-//         option2.removeEventListener('click', clickHandler);
-//         option3.removeEventListener('click', clickHandler);
-    
-// }
-
-
-// function nextQuestion (event2) {
-//     var id = event2.target.id;
-//     if (id = 'next_button'){
-//         var h2Element = document.getElementById('question');
-//         h2Element.remove();
-//         var cropImage = document.getElementById('crop_image');
-//         cropImage.remove();
-//         var ulElement = document.getElementById('ul_element');
-//         ulElement.remove();
-//         var answer = document.getElementById('answer');
-//         answer.remove();
-//         var fullImage = document.getElementById('full_image');
-//         fullImage.remove();
-//         var nextButton = document.getElementById('next_button');
-//         nextButton.remove();
-//     }
-//     getQuestion();
-//     getOptions();
-// }
-
-
-
+quizContainer.addEventListener('submit', submitHandler);
